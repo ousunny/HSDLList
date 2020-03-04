70a660a85c7298c3e84d7c67b07bf9f9d3389ccc
Sunny <ousunny91@gmail.com>
2020-03-04 10:06:33 -0500

    Setup DB

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 
+// Connect Database
+connectDB();
+
+// Middleware
 app.use(
   express.json({
     extended: false
