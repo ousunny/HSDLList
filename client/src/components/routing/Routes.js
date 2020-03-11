ae829c704e55586ae2b3f46c4abfa59e5e3f3eb0
Sunny <ousunny91@gmail.com>
2020-03-11 18:11:41 -0400

    Setup and implement alerts

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Alert from '../layout/Alert';
 import Login from '../auth/Login';
 import Register from '../auth/Register';
 
 const Routes = () => {
   return (
     <Fragment>
+      <Alert />
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
