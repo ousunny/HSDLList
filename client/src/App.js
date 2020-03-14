d0ebcb86684d6894ccf011780aebf92ac8072711
Sunny <ousunny91@gmail.com>
2020-03-13 22:19:17 -0400

    Create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Routes from './components/routing/Routes';
+import NavBar from './components/layout/NavBar';
+
+import setAuthToken from './utils/setAuthToken';
+import { loadUser } from './actions/auth';
 
 import { Provider } from 'react-redux';
 import store from './store';
 
 import './App.css';
 
+localStorage.token && setAuthToken(localStorage.token);
+
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
+          <NavBar />
           <Switch>
             <Route component={Routes} />
           </Switch>
---
+++ client/src/components/layout/NavBar.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Link,
+  Button
+} from '@material-ui/core';
+
+import { Add, List, Person } from '@material-ui/icons';
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    flexGrow: 1
+  },
+  marginRight: {
+    marginRight: theme.spacing(2)
+  },
+  link: {
+    color: 'inherit'
+  },
+  button: {
+    color: 'inherit'
+  }
+}));
+
+const NavBar = ({ auth: { isAuthenticated } }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h5" className={classes.title}>
+          <Link
+            href="/"
+            className={classes.link}
+            style={{ textDecoration: 'none' }}
+          >
+            HSDLList
+          </Link>
+        </Typography>
+        {isAuthenticated ? (
+          <div>
+            <IconButton className={classes.button}>
+              <Add />
+            </IconButton>
+            <IconButton className={classes.button}>
+              <List />
+            </IconButton>
+            <IconButton className={classes.button}>
+              <Person />
+            </IconButton>
+          </div>
+        ) : (
+          <div>
+            <Button href="/register" className={classes.button}>
+              Register
+            </Button>
+            <Button href="/login" className={classes.button}>
+              Login
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+NavBar.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  {}
+)(NavBar);
